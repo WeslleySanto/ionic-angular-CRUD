3c2760c0275cd1f806b9f155abd66914a3421c2a
Weslley Santo <weslley.santo@gmail.com>
2017-07-31 21:58:40 -0300

    removendo arquivos não utilizados | IONIC [3]- MODAL DE CADASTRO(BÁSICO) | IONIC [4] - FORMULÁRIO DE CADASTRO | IONIC [5] - CADASTRO DE USUÁRIOS

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,6 @@
 angular.module('starter.controllers', [])
 
-.controller('AppCtrl', function($scope, Data) {
+.controller('AppCtrl', function($scope, Data, $ionicModal) {
 
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
@@ -17,19 +17,47 @@ angular.module('starter.controllers', [])
   $scope.usuarios_lista = [];
 
   var getData = function(){
-     var params = {
-        counter: $scope.usuarios_lista.length,
-        token: '1f3d2gs3f2fg3as2fdg3re2t1we46er45'
-     };
+   var params = {
+    counter: $scope.usuarios_lista.length,
+    token: '1f3d2gs3f2fg3as2fdg3re2t1we46er45'
+  };
 
-     Data.getData(params).success(function(data){
-        $scope.usuarios_lista = data;
-     }).error(function(data){
-        console.log(data ? data : 'Não foi possível acessar o servidor');
-     });
+  Data.getData(params).success(function(data){
+    $scope.usuarios_lista = data;
+  }).error(function(data){
+    console.log(data ? data : 'Não foi possível acessar o servidor');
+  });
 
+};
+
+  $ionicModal.fromTemplateUrl('templates/cadastro.html',{
+    scope:$scope,
+    animation: 'slide-in-up'
+  }).then(function(modal){
+    $scope.modal = modal;
+  });
+
+  $scope.abreCadastro = function(){
+    $scope.modal.show();
+  };
+
+  var fecharCadastro = $scope.fechaCadastro = function(){
+    $scope.modal.hide();
   };
 
   getData();
+
+  $scope.cadastroUsuario = function(usuario){
+    Data.setData(usuario)
+    .success(function(data){
+        alert(data);
+        fecharCadastro();
+        getData();
+    }).error(function(data){
+        alert(data)
+    });
+
+    console.log(usuario);
+  };
 
 });
--- www/js/services/app_services.js
+++ www/js/services/app_services.js
@@ -3,19 +3,29 @@
 
   angular.module("starter").value("Config", {
 
-    getUrl: "http://weslleyesanto.com.br/api/ionicAngularCRUD/"
+    //getUrl: "http://weslleyesanto.com.br/api/ionicAngularCRUD/"
+    getUrl: "http://localhost/apiIonicAngularCRUD/"
   });
 
   angular.module("starter").service("Data", function($http, Config){
+
     //recuperação de dados
     this.getData = function(params){
       return $http({
         method: "POST",
         url: Config.getUrl + "apiRecupera.php",
         data: params,
-        headers : {
-                        'Content-Type' : 'application/x-www-form-urlencoded; charset=UTF-8'
-                    }
+        headers : { 'Content-Type' : 'application/x-www-form-urlencoded; charset=UTF-8'}
+      });
+    };
+
+    //CADASTRO
+    this.setData = function(dados){
+      return $http({
+        method: "POST",
+        url: Config.getUrl + "apiCadastro.php",
+        data: dados,
+        headers : { 'Content-Type' : 'application/x-www-form-urlencoded; charset=UTF-8'}
       });
     };
     
